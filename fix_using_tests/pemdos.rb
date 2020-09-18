4ef0b56175634e90314ea66e352ad4a9cb812e21
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-18 17:40:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,12 @@
 # don't forget to add: require 'pry'
-require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10.times do
-      puts "s"
-      end
-      + string
+    10.times do 
+      "s" 
+    end
+    + string
+  else
+    string
   end
 end
